976f97b946ec3dd1b93a7b6edc67d120b4805c26
Kasper1999 <sasha.kasper12@mail.ru>
2018-09-20 19:11:24 +0300

    Love triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,25 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var k=[3, 9, 12, 6, 13, 16, 15, 3, 14, 5, 4, 1, 10, 2, 4, 4, 10, 14, 11];
+
+  var z=[]; 
+  var j=0;
+  var povt=0; var r=0;  
+  var first;
+  var tr=0;
+  for(var l=0;l<preferences.length;l++){
+    first=l+1;
+      for (var i=l;tr==0 && r<preferences.length;i=j-1)
+    {
+     
+     j=preferences[i];
+     r++;
+     if(preferences[i]!=i+1){
+     if(preferences[i]==first) {tr++; povt+=tr;}}
+     
+    }tr=0;r=0;
+    }
+  
+    return Math.trunc(povt/3);
 };
